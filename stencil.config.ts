6e1ded226e97f01e5180cee0a7ae2d6bfefb6692
Dani2002181716 <91689782+Dani2002181716@users.noreply.github.com>
2024-08-18 12:27:48 -0500

    Add files via upload

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,7 +1,7 @@
 import { Config } from '@stencil/core';
 
 export const config: Config = {
-  namespace: 'componente-danielacoello',
+  namespace: 'api-componentedaniela',
   outputTargets: [
     {
       type: 'dist',
